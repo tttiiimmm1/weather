30a524c1aff186924f8715bbe47d16afbcdd6fc6
Tim Engeman <tim.engeman@gmail.com>
2021-09-04 23:04:32 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+//import cloudy from './Images/cloudy-bg.jpg';
+//import stormy from './Images/stormy-bg.jpg';
+//import sunny from './Images/sunny-bg.jpg';
+import rainy from './Images/rainy-bg.PNG';
+//import snowy from './Images/snowy-bg.jpg';
+//import foggy from './Images/foggy-bg.jpg';
+
+function App() {
+  return (
+    <div className="background">
+      <div className="upper-bar"/>
+
+        <img src={rainy} className="cloudy-bg" alt=""/>
+    </div>
+  );
+}
+
+export default App;
